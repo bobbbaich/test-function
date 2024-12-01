39548c5c9129dd217f5a3310ccc27073606ede22
bohdanpohorilyi <bohdan.pohorilyi@base.ai>
2024-12-01 20:54:37 +0100

    fix gson issue

--- src/main/java/com/example/azure/web/AzureWebDemoApplication.java
+++ src/main/java/com/example/azure/web/AzureWebDemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.azure.web;
 
+import com.google.gson.GsonBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class AzureWebDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AzureWebDemoApplication.class, args);
+    }
+
+    @Bean
+    public GsonBuilder gsonBuilder() {
+        return new GsonBuilder();
     }
 
 }
